149a97b19ac0564f3be9064574539c918be9f249
instabytemedia <74257666+instabytemedia@users.noreply.github.com>
2026-02-20 16:51:50 +0100

    Add components/ui/label.tsx

---
+++ components/ui/label.tsx
@@ -0,0 +1,14 @@
+import { forwardRef, type LabelHTMLAttributes } from "react";
+
+export interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {}
+
+export const Label = forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className = "", ...props }, ref) => (
+    <label
+      ref={ref}
+      className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${className}`}
+      {...props}
+    />
+  )
+);
+Label.displayName = "Label";
